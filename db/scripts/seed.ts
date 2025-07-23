f237f6b530bf2ad0ffab044e785b421f997c168a
haikalardzi <haikalardzi@gmail.com>
2025-07-23 13:33:20 +0700

    fix seeding

--- db/scripts/seed.ts
+++ db/scripts/seed.ts
@@ -3,7 +3,7 @@ import { Client } from 'pg';
 import { chatbotQuestions, chatbotResponses } from '../schema';
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
 });
 
 const db = drizzle(client);
